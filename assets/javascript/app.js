2a6b9432c837219eeeec3335152f92cc186c74c8
Yoon Lee <yoonlee@Yoons-MacBook-Air.local>
2017-02-27 18:20:33 -0800

    set up clickable choices and feedback re: wrong/right answer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,53 @@
 var question = {
-	"question": "",
+	"question": "Sample Question",
 	"choices": [
-		"",
-		"",
-		"",
-		""
+		"A",
+		"B",
+		"C",
+		"D"
 	],
-	"answer": ""
+	"answer": "A"
 };
 
 console.log(question);
+
+// render question
+var questionElement = $("<div>").text(question["question"]);
+$(".container").append(questionElement);
+
+// render choices
+for(var i = 0; i < question["choices"].length; i++) {
+	var choiceElement = $("<div>").text(question["choices"][i]);
+	$(".container").append(choiceElement);
+	choiceElement.addClass("choice");
+	// if(question["choices"][i] === question["answer"]) {
+	// 	choiceElement.addClass("answer");	
+	// }
+	choiceElement.attr("id", question["answer"]);
+}
+
+// listens for choice selection
+$(".choice").on("click", function() {
+	console.log($(this).text());
+	if($(this).text() === $(this).attr("id")) {
+		// RIGHT ANSWER
+		var messageElement = $("<div>").text("Correct!");
+		$(".container").append(messageElement);
+	} else {
+		// WRONG ANSWER
+		var messageElement = $("<div>").text("Nope!");
+		$(".container").append(messageElement);
+		var correctAnswerElement = $("<div>").text("The Correct Answer was: " + $(this).attr("id"));
+		$(".container").append(correctAnswerElement);
+	}
+});
 
 $("#button-start").on("click", function() {
 	$("#button-start").css("display", "none");
 	var seconds = 0;
 	var timeLimit = 30;
 	var timeRemaining = timeLimit - seconds;
-	$("#timer").html(timeRemaining + " seconds remaining")
+	$("#timer").html(timeRemaining + " seconds remaining");
 	console.log(timeRemaining);
 	var timer = setInterval(function() {
 		seconds++;
